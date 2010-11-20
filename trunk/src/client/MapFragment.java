729fe51a630838c04457c74d8e6ce0259a59998f
sorcchan <sorcchan@58aaa60e-a7a2-11de-8c46-7bd654735883>
2010-11-20 17:10:52 +0000

    oops

---
+++ trunk/src/client/MapFragment.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package client;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Sorc
+ */
+public class MapFragment {
+    private static int width;
+
+    public static void setHeight(int height) {
+        MapFragment.height = height;
+    }
+
+    public static void setWidth(int width) {
+        MapFragment.width = width;
+    }
+    private static int height;
+
+    public static int getHeight() {
+        return height;
+    }
+
+    public static int getWidth() {
+        return width;
+    }
+
+    private final int[][] hmap;
+    private final int idx;
+
+    public int getIdx() {
+        return idx;
+    }
+
+    public int getIdy() {
+        return idy;
+    }
+    private final int idy;
+    private BufferedImage img = null;
+
+    public MapFragment(int idx, int idy, int [][] hmap) {
+        this.hmap = hmap;
+        this.idx = idx;
+        this.idy = idy;
+    }
+
+    public BufferedImage getImage() {
+        if (img == null) {
+            img = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR_PRE);
+            Graphics g = img.getGraphics();
+            g.setColor(new Color((float) 0.1, (float) 1.0, (float) 0.3, (float) 0.7));
+            g.fillRect(0, 0, width - 1, height - 1);
+            g.setColor(Color.GREEN);
+        }
+        
+        return img;
+    }
+}
